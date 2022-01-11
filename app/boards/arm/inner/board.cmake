b9b12194b8b739e266add9bc5e81829db88778be
Sets3n <li.xuebin@outlook.com>
2022-01-11 15:15:41 +0800

    add inner shield

--- app/boards/arm/inner/board.cmake
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2021 The ZMK Contributors
-# SPDX-License-Identifier: MIT
-# iNNER (with eByte E73-2G4M08S1C nrf52840 System-on-module) configuration
-
-set(OPENOCD_NRF5_SUBFAMILY nrf52)
-board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
-include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
